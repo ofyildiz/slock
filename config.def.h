fb1dbf34aa2313936667720963fb8a0fed2443b0
Oemer Yildiz <oemer.yildiz@tuhh.de>
2020-09-21 19:50:57 +0200

    dracula theme

--- config.def.h
+++ config.def.h
@@ -3,9 +3,9 @@ static const char *user  = "nobody";
 static const char *group = "wheel";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[INIT] =   "#282a36",   /* after initialization */
+	[INPUT] =  "#bd93f9",   /* during input */
+	[FAILED] = "#ff5555",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
